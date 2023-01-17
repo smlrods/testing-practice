e047f8112d490d7cbe476e313ee9935dee54494d
sml <smlrodrigues.dev@gmail.com>
2023-01-17 07:57:07 -0300

    Refactor function capitalize

--- string-functions.js
+++ string-functions.js
@@ -2,7 +2,8 @@ export function capitalize(string) {
   const array = string.split('');
   const result = [array[0].toUpperCase()];
   array.forEach((element, index) => {
-    index ? result.push(element.toLowerCase()) : null;
+    if (index) return;
+    result.push(element.toLowerCase());
   });
   return result.join('');
 }
