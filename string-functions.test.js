b7746ec68673eeca6dd3c43ea8b9dc6a5e27e48d
sml <smlrodrigues.dev@gmail.com>
2023-01-17 08:13:53 -0300

    Add test for reverseString

--- string-functions.test.js
+++ string-functions.test.js
@@ -1,8 +1,16 @@
 import { capitalize } from './string-functions.js';
 
+const strings = ['sometext', 'javascript', 'github', 'JeSt', 'eSlint']
+const stringsReverse = ['txetemos', 'tpircsavaj', 'buhtig', 'tSeJ', 'tnilSe'];
+
 test('first character capitalized', () => {
-  const strings = ['sometext', 'javascript', 'github', 'JeSt', 'eSlint']
   strings.forEach(element => {
     expect(capitalize(element)).toMatch(/^[A-Z][a-z]+$/);
   });
+});
+
+test('reverse string', () => {
+  for(let i = 0; i < strings.length; i++) {
+    expect(reverseString(strings[i])).toBe(stringsReverse[i]);
+  }
 });
